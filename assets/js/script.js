cb653443322d0ac67a67039dce6a4e29047568d9
Dakota Dunn <dakotadunn5@gmail.com>
2022-01-11 22:06:41 -0800

    made the value's for the drop down and the input form readable on the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,23 @@
 var formEl = document.querySelector('#task-form')
 var tasksToDoEl = document.querySelector('#tasks-to-do');
+
+
 var createTaskHandler = function(event) {
     event.preventDefault();
-
+    var taskNameInput = document.querySelector("input[name = 'task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    // create div to hold task info and add to list item
+    var taskInfoEl = document.createElement('div');
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class = 'task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    
     tasksToDoEl.appendChild(listItemEl);
   }
   
-
 formEl.addEventListener('submit', createTaskHandler);
